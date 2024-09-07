86160669a8ad122dc6da4460f21c4e1e291e9b22
Alex <37157369+alex938@users.noreply.github.com>
2024-09-07 15:13:46 +0100

    Update settings.py

--- worldCountries/settings.py
+++ worldCountries/settings.py
@@ -134,5 +134,5 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 CORS_ORIGIN_ALLOW_ALL = False
 CORS_ORIGIN_WHITELIST = (
     'http://localhost:8080',
-    'http://192.168.244.132:8000',  # If your app is served on a specific port
+    'http://192.168.244.132',  # If your app is served on a specific port
 )
